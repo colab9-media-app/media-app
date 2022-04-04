6931cec835caff7ad1b7cd70e6c9265c70d45448
CalAkler <calvinakler@gmail.com>
2022-04-04 15:05:53 -0400

    modal api call & details

--- src/components/Search/DetailsCard/DetailsCard.js
+++ src/components/Search/DetailsCard/DetailsCard.js
@@ -1,39 +1,79 @@
 import './detailsCard.css';
 import logoTMDB from '../../../assets/images/logoOneLineTMDB.svg';
+import moment from 'moment';
 
-const DetailsCard = ({ show, setShow, id, details }) => {
+const DetailsCard = ({ show, setShow, details, detailsError }) => {
   return (
     !show ? null :
-
-      <div className="modal" key={id}>
-        <button onClick={() => setShow(false)}>X</button>
-        <div className="cardLeft">
-          <div className="poster">
-            <img
-              src={`https://image.tmdb.org/t/p/w500/${details.poster_path}`}
-              alt={`Poster for ${details.original_title}`}
-            />
+      <div className="modal">
+        <button className="closeButton" onClick={() => setShow(false)}>X</button>
+        <div className="cardInfo">
+          <div className="cardLeft">
+            <div className="poster">
+              <img
+                src={`https://image.tmdb.org/t/p/w500/${details.poster_path}`}
+                alt={`Poster for ${details.original_title}`}
+              />
+            </div>
+            
           </div>
-          <div className="cardLogo">
-            <img src={logoTMDB} alt="" />
-            <p>{details.vote_average}</p>
-          </div>
-        </div>
-        <div className="cardRight">
+          <div className="cardRight">
           {
-            details.media_type === "movie" ?
-              <h3>{details.title}</h3> :
-              <h3>{details.name}</h3>
+            details.title ?
+              <h3>{details.title}</h3>
+            :
+              <div className="tvTitle">
+                <h3>{details.name}</h3>
+                <p>{moment(details.first_air_date).format('YYYY')}</p>
+              </div>
           }
           <p>{details.overview}</p>
-          <h4>Genre</h4>
-          
-        </div>
+          <h4>Genres</h4>
+          <ul className="genre">
+            {
+              details.genres?.map((genre) => {
+                return (
+                  <li key={genre.id}>{genre.name}</li>
+                )
+              })
+            }
+          </ul>
+          {
+            details.seasons ?
+              <div className="numberDetails">
+                <div className="seasonNum">
+                  <h4>N° of seasons</h4>
+                  <p>{details.number_of_seasons}</p>
+                </div>
+                <div className="episodeNum">
+                  <h4>N° of episodes</h4>
+                  <p>{details.number_of_episodes}</p>
+                </div>
+                {
+                  details.next_episode_to_air !== null ?
+                    <div className="airDate">
+                      <h4>Next episode airing</h4>
+                      <p>{moment(details.next_episode_to_air.air_date).format('MMMM D, YYYY')}</p>
+                    </div>
+                  : null
+                }
+              </div>
+            :
+              <div className="dateDetails">
+                <h4>Release Date</h4>
+                <p>{moment(details.release_date).format('MMM D, YYYY')}</p>
+              </div>
 
-        
+
+          }
+        </div>
+        </div>
+        <div className="cardBottom">
+          <img src={logoTMDB} alt="Logo for TMDB." />
+          <p>{details.vote_average}</p>
+        </div>
       </div>
   )
-
 }
 
 export default DetailsCard;
--- src/components/Search/Results.js
+++ src/components/Search/Results.js
@@ -9,9 +9,9 @@ const Results = props => {
   const [index, setIndex] = useState('');
   const [mediaID, setMediaID] = useState('');
   const [details, setDetails] = useState([]);
+  const [detailsError, setDetailsError] = useState(false);
 
   const runDetailsSearch = (id, index) => {
-
     if (props.result[index].media_type === 'movie') {
       axios({
         url: `https://api.themoviedb.org/3/movie/${id}`,
@@ -21,35 +21,37 @@ const Results = props => {
           original_language: 'en',
         }
       }).then(res => {
-        // const data = res.data.results;
-        console.log(res.data);
-        // if (res.status === 200 && data.length > 0) {
-        //   const filteredData = data.filter((content) => {
-        //     if (content.poster_path !== null) {
-        //       return content.media_type === "movie" || content.media_type === "tv";
-        //     }
-        //   });
-        //   setData(filteredData);
-        //   setMediaResult(filteredData);
-        //   console.log(mediaResult);
-        }) 
-      //   else {
-      //     throw Error();
-      //   }
-      // }).catch(() => {
-      //   setErrorStatus(true);
-      // })
-    // } else {
-
+        if (res.status === 200) {
+          setDetails(res.data);
+          console.log(res.data);
+        } else {
+          throw Error();
+        }
+      }).catch(() => {
+        setDetailsError(true);
+      });
+    } else if (props.result[index].media_type === 'tv') {
+      axios({
+        url: `https://api.themoviedb.org/3/tv/${id}`,
+        params: {
+          api_key: process.env.REACT_APP_API_KEY,
+          include_adult: false,
+          original_language: 'en',
+        }
+      }).then(res => {
+        if (res.status === 200) {
+          setDetails(res.data);
+          console.log(res.data);
+        } else {
+          throw Error();
+        }
+      }).catch(() => {
+        setDetailsError(true);
+      });
     }
+    setShow(true);
+    
   }
-
-  // const openModal = (index, id) => {
-  //   setShow(true);
-  //   setIndex(index);
-  //   setMediaID(id)
-  //   runDetailsSearch(index, mediaID);
-  // }
 
   return (
     <>
@@ -60,17 +62,18 @@ const Results = props => {
             <p>No results found for your search, please search again.</p>
           </div> :
           <ul className="resultContainer">
-            {/* {
+            {
               show ?
                 <DetailsCard
                   show={show}
                   setShow={setShow}
-                  index={index}
-                  id={mediaID}
-                  details={props.result[mediaID]}
+                  // index={index}
+                  // id={mediaID}
+                  details={details}
+                  error={detailsError}
                 />
-                : null
-            } */}
+              : null
+            }
             {
               props.result.map((media, index) => {
                 return (
